6028752f5fbd8db75e7b61b691f3f1c214c4ddf9
Raimondo Terry <terry.raimondo@gmail.com>
2012-11-16 17:01:16 +0100

    Adding news

--- Symfony/src/MineDoc/HomeBundle/Controller/AdminController.php
+++ Symfony/src/MineDoc/HomeBundle/Controller/AdminController.php
@@ -97,7 +97,7 @@ class AdminController extends Controller
             if ($test_form->isValid() && $picture_form->isValid()) {
                 $news->setPicture($testpic);
                 $em = $this->getDoctrine()->getEntityManager();
-                if (getimagesize("upload/" . $testpic->getName() . "/" . $testpic->getFile())['mime'] != "image/png" && getimagesize("upload/" . $testpic->getName() . "/" . $testpic->getFile())['mime'] != "image/jpeg") {
+                if (getimagesize("upload/" . $testpic->getName() . "/" . $testpic->getFile())['mime'] != "image/png" && getimagesize($testpic->getFile())['mime'] != "image/jpeg") {
                     $this->get('session')->setFlash('warning', 'Type d\'image invalide !');
                     return $this->redirect($this->generateUrl('newsadmin'));
                 }
