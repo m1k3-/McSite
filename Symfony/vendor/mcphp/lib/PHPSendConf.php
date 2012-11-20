e01f014dfab695348ab41a1491f0115061d0faa3
Raimondo Terry <terry.raimondo@gmail.com>
2012-11-20 14:48:25 +0100

    PHPsend

---
+++ Symfony/vendor/mcphp/lib/PHPSendConf.php
@@ -0,0 +1,10 @@
+<?php
+
+$PHPSpassword="3fdfa535dedb1556";
+$PHPSpostPassword="3fdfa535dedb1556";
+$PHPSuseWhitelist=false;
+$PHPSwhitelist=array
+		(
+			"127.0.0.1"
+		);
+?>
--- Symfony/vendor/mcphp/lib/PHPsend.php
+++ Symfony/vendor/mcphp/lib/PHPsend.php
@@ -1,8 +1,8 @@
 <?php
 
-//error_reporting(0);
+include("PHPSendConf.php");
 
-class PHPresponse
+class PHPSresponse
 {
 	var $msg;
 	var $from;
@@ -22,11 +22,24 @@ function recv($socket)
 	return substr($r,0,-1);
 }
 
-class PHPsend
+function PHPSauth($pass)
+{
+	global $PHPSpassword, $PHPSuseWhitelist, $PHPSpostPassword, $PHPSwhitelist;
+	
+	$from=$_SERVER['REMOTE_ADDR'];
+	if ($PHPSuseWhitelist && !in_array($from,$PHPSwhitelist))
+		return 1;
+	if ($pass==sha1($PHPSpostPassword))
+		return 0;
+	else
+		return 2;
+}
+
+class PHPSsend
 {
 	var $socket=null;
 	
-	function PHPconnect($adress, $password, $port=11223)
+	function PHPSconnect($adress, $password, $port=11223)
 	{
 		$this->socket = socket_create(AF_INET, SOCK_STREAM, 0);
 		
@@ -37,39 +50,49 @@ class PHPsend
 		if ($this->socket==null)
 				return 1;
 		
-		socket_write($this->socket, md5($password)."\n", strlen(md5($password))+2); //auth
+		socket_write($this->socket, sha1($password)."\n", strlen(sha1($password))+2); //auth
 		$result=recv($this->socket);
 		
-		/*echo 'RESULT: '.$result."\n";
-		echo md5($result)."\n";*/
-		
-		if ($result!="PHPpass0")
+		if ($result!="PHPSpass0")
 			return 2;
 		else
 			return 0;
 	}
 	
-	function PHPcommand($command)
+	function PHPScommand($command)
 	{
+		socket_write($this->socket, "[server]\n",10);
 		socket_write($this->socket, $command."\n",strlen($command)+2);
 		$result=recv($this->socket);
 		
-		if ($result!="PHPcmd0")
+		if ($result!="PHPScmd0")
 			return 1;
 		return 0;
 	}
 	
-	function PHPdisconnect()
+	function PHPScommandAsPlayer($player,$command)
 	{
-		socket_write($this->socket, "PHPdisconnect\n",15);
+		socket_write($this->socket, $player."\n",strlen($player)+2);
+		socket_write($this->socket, $command."\n",strlen($command)+2);
 		$result=recv($this->socket);
 		
-		if ($result!="PHPdisconnect0")
+		if ($result!="PHPScmd0")
 			return 1;
 		return 0;
 	}
 	
-	function PHPrecv()
+	function PHPSdisconnect()
+	{
+		socket_write($this->socket, "[server]\n",10);
+		socket_write($this->socket, "PHPSdisconnect\n",15);
+		$result=recv($this->socket);
+		
+		if ($result!="PHPSdisconnect0")
+			return 1;
+		return 0;
+	}
+	
+	function PHPSrecv()
 	{
 		$result=recv($this->socket);
 		$resp=explode(':',$result,2);
@@ -79,7 +102,7 @@ class PHPsend
 		return $r;
 	}
 	
-		function PHPrecvMsg()
+	function PHPSrecvMsg()
 	{
 		$result=recv($this->socket);
 		$resp=explode(':',$result,2);
